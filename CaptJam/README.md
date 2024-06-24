a20eaf486ea4aa9327a0ba0b306ad788b6c9499e
Boog <dentldamn@gmail.com>
2024-06-24 02:01:25 -0700

    Initial commit

---
+++ CaptJam/README.md
@@ -0,0 +1,7 @@
+# Hook-Jab-Uppercut
+
+A starter program for writing a rock, paper, scissors game
+
+## Usage
+
+1.  Include in HTML file to run a simple game to play against the CPU
---
+++ CaptJam/rps.js
@@ -0,0 +1,7 @@
+/*
+    Jab-hook-upper: rock, paper, scissors format
+    Author: BoogDoesIt
+    Version: 1.0
+    Rules/Instructions: hook beats jab, jab beats uppercut, uppercut beats hook
+    
+*/
