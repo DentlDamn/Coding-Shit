8edb945b81ed62f7eb3a3d79c60cfbaef872f8c5
Boog <dentldamn@gmail.com>
2024-06-24 16:33:21 -0700

    playerScore is concatting to playerrChoice but error outside of function

--- CaptJam/rps.js
+++ CaptJam/rps.js
@@ -6,60 +6,69 @@
     
 */
 
-function playGame() {
-  /*let jab = document.getElementById("jab").innerHTML;
-  let hook = document.getElementById("hook").innerHTML;
-  let upper = document.getElementById("upper").innerHTML;*/
-  var choices = ["jab", "hook", "upper"];
-  let randomPunch = choices[Math.floor(Math.random() * choices.length)];
-  document.getElementById("jab").onclick = function () {
-    let playerChoice = "jab";
-    console.log(playerChoice);
-  };
-  document.getElementById("hook").onclick = function () {
-    let playerChoice = "hook";
-    console.log(playerChoice);
-  };
-  document.getElementById("upper").onclick = function () {
-    let playerChoice = "upper";
-    console.log(playerChoice);
-  };
-  winner(randomPunch, playerChoice);
-}
+var playerScore = 0;
+var cpuScore = 0;
 
-function winner(randomPunch, playerChoice) {
-  if (randomPunch === "jab" && playerChoice === "hook") {
-    playerScore++;
-  } else if (randomPunch === "jab" && playerChoice === "upper") {
-    cpuScore++;
-  } else if (randomPunch === "jab" && playerChoice === "jab") {
-    console.log("It's a Tie!");
-  } else if (randomPunch === "hook" && playerChoice === "jab") {
-    cpuScore++;
-  } else if (randomPunch === "hook" && playerChoice === "upper") {
-    playerScore++;
-  } else if (randomPunch === "hook" && playerChoice === "hook") {
-    console.log("It's a Tie!");
-  } else if (randomPunch === "upper" && playerChoice === "hook") {
-    cpuScore++;
-  } else if (randomPunch === "upper" && playerChoice === "jab") {
-    playerScore++;
-  } else if (randomPunch === "upper" && playerChoice === "upper") {
-    console.log("It's a Tie!");
-  }
-  game();
-
+game();
 function game() {
-  let playerScore = 0;
-  let cpuScore = 0;
   if (playerScore || cpuScore < 3) {
     playGame();
   } else {
     refresh();
   }
 }
+
+function playGame() {
+  var choices = ["jab", "hook", "upper"];
+  let randomPunch = choices[Math.floor(Math.random() * choices.length)];
+  document.getElementById("jab").onclick = function () {
+    var playerChoice = "jab";
+    console.log(playerChoice + playerScore);
+    console.log(randomPunch);
+  };
+  document.getElementById("hook").onclick = function () {
+    var playerChoice = "hook";
+    console.log(playerChoice + playerScore);
+    console.log(randomPunch);
+  };
+  document.getElementById("upper").onclick = function () {
+    var playerChoice = "upper";
+    console.log(playerChoice + playerScore);
+    console.log(randomPunch);
+  };
+  winner(randomPunch, playerChoice);
+}
 playGame();
+
 function winner(randomPunch, playerChoice) {
+  if (randomPunch === "jab" && playerChoice === "hook") {
+    playerScore++;
+    console.log("One point to you!");
+  } else if (randomPunch === "jab" && playerChoice === "upper") {
+    cpuScore++;
+    console.log("One point to the CPU!");
+  } else if (randomPunch === "jab" && playerChoice === "jab") {
+    console.log("It's a Tie!");
+  } else if (randomPunch === "hook" && playerChoice === "jab") {
+    cpuScore++;
+    console.log("One point to the CPU!");
+  } else if (randomPunch === "hook" && playerChoice === "upper") {
+    playerScore++;
+    console.log("One point to you!");
+  } else if (randomPunch === "hook" && playerChoice === "hook") {
+    console.log("It's a Tie!");
+  } else if (randomPunch === "upper" && playerChoice === "hook") {
+    cpuScore++;
+    console.log("One point to the CPU!");
+  } else if (randomPunch === "upper" && playerChoice === "jab") {
+    playerScore++;
+    console.log("One point ot you!");
+  } else if (randomPunch === "upper" && playerChoice === "upper") {
+    console.log("It's a Tie!");
+  }
+  winner(randomPunch, playerChoice);
+}
+/*function winner(randomPunch, playerChoice) {
   if (randomPunch === "jab" && playerChoice === "hook") {
     playerScore++;
   } else if (randomPunch === "jab" && playerChoice === "upper") {
@@ -83,8 +92,4 @@ function winner(randomPunch, playerChoice) {
 winner();
 /*jab.document.addEventListener("onclick", playGame);
 let you = document.getElementById("score0");
-let cpu = document.getElementById("score1");
-
-var choices = ["jab", "hook", "upper"];
-let randomPunch = choices[Math.floor(Math.random() * choices.length)];
-playGame();*/
+let cpu = document.getElementById("score1");*/
